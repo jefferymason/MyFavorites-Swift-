bbd8195d5670b8a6477d07a54b0add8bc2e7a94d
Jeffery Wayne Mason <jefferymason72@gmail.com>
2021-01-18 12:14:45 -0600

    Ready for App Challenge

--- MyFavorites/TextViews.swift
+++ MyFavorites/TextViews.swift
@@ -38,7 +38,7 @@ struct AnswerText: View {
         Text(text)
             .font(.largeTitle)
             .fontWeight(.bold)
-            .foregroundColor(Color("Dark"))
+            .foregroundColor(Color("Black"))
         Spacer()
         
     }
